2845eb2d0d6c7821d8670b7ea09282c0652f0187
Seth Glenn <Sbgees@yahoo.com>
2021-03-22 21:32:05 -0500

    adding boiler plate code for server side initialization

---
+++ server/routes/html/html.js
@@ -0,0 +1,14 @@
+// Requiring path to so we can use relative routes to our HTML files
+const path = require('path');
+
+// const db = require('../../models');
+const router = require('express').Router();
+
+
+// Routes the user to report a crime
+router.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/pages/index.html'));
+});
+
+
+module.exports = router;
