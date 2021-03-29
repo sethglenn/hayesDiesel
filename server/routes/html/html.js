99adfb55b7882fb8eb2b419e7b7de8e6aa9a6793
Seth Glenn <Sbgees@yahoo.com>
2021-03-28 20:30:26 -0500

    edited db name in config

--- server/routes/html/html.js
+++ server/routes/html/html.js
@@ -1,14 +1,16 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require('path');
 
-// const db = require('../../models');
 const router = require('express').Router();
 
 
-// Routes the user to report a crime
-router.get('/', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../pages/index.html'));
+// Routes the user to pages
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../pages/index.html'));
 });
 
+router.get('/submission', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../pages/submission.html'));
+});
 
 module.exports = router;
